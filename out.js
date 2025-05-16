05c9ef2392b6c46a612438259e781f8bae9b6cc5
DiuANg <202187491+DiuANg@users.noreply.github.com>
2025-05-16 17:31:14 +0700

    userChoice to logic connect

--- out.js
+++ out.js
@@ -1,28 +1,34 @@
 console.log("get ready! Rock, paper or scissors?")
 let message;
 // ctr-/ to comment
-// create variables 
-
-
-computerChoice = "ROCK";
-
-
-// get input from user store it in variable userChoice (ALL UPPERCASE)
-let userChoice = prompt("Rock, paper or scissors?",);
-userChoice = userChoice.toUpperCase();
-// function for computer's choice, store it in computerChoice
 // let variables userScore and computerScore =0
 let userScore=0;
 let computerScore=0;
-// compare userChoice and computerChoice
+// create variables (testing feitj)
+
+
+computerChoice = "PAPER";
+
+
+// get input from user store it in variable userChoice (ALL UPPERCASE)
+function getUserChoice() {
+let userChoice = prompt("Rock, paper or scissors?",);
+return userChoice.toUpperCase();
+}
+// function for computer's choice, store it in computerChoice
+
+
+// Win-lose logic 1 round
 //    if userChoice the same as computerChoice: log "It's a tie".
 //    else 
-//      switch computerChoice is "ROCK"
-//         userChoice is "PAPER": log "1 point to u", userScore add 1. break;
-//         userChoice is "SCISSORS": log "1 point to me", computerScore add 1. break;
-
+//       computerChoice is "ROCK"
+//         userChoice is "PAPER": log "1 point to u", userScore add 1
+//         userChoice is "SCISSORS": log "1 point to me", computerScore add 1 (...)
+function roundLogic(userChoice,computerChoice){
+// log computerChoice for user to see(combine to playRound later)
+console.log(computerChoice+"!!")
 if (userChoice===computerChoice) {
-    console.log("It's a tie.");
+    console.log("It's a tie...");
     console.log("Your Score:"+userScore);
     console.log("My Score:"+computerScore);
 } else if (computerChoice==="ROCK") {
@@ -56,3 +62,25 @@ if (userChoice===computerChoice) {
     console.log("Your Score:"+userScore);
     console.log("My Score:"+computerScore);
 }
+}
+// combine into one round
+
+// combine all logic into 1
+function playGame() {
+    userChoice=getUserChoice();
+    roundLogic(userChoice,computerChoice);
+    userChoice=getUserChoice();
+    roundLogic(userChoice,computerChoice);
+    userChoice=getUserChoice();
+    roundLogic(userChoice,computerChoice);
+    userChoice=getUserChoice();
+    roundLogic(userChoice,computerChoice);
+    userChoice=getUserChoice();
+    roundLogic(userChoice,computerChoice);
+    message= userScore>computerScore? "YOU WON :))" :
+             userScore==computerScore? "TIE!!":
+             "YOU LOST :((";
+    console.log(message);
+}
+// run the game
+playGame();
